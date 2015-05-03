b1d6b81fb7b20c9893d6435be1fe8317450f00bb
Robert Lord <robert@lord.io>
2015-05-03 14:29:41 -0500

    Add basic Stripe checkout and payment

--- server.rb
+++ server.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
 require 'stripe'
 
+Stripe.api_key = ENV["STRIPE_API_KEY"]
+
 class Cashin < Sinatra::Base
   get '/' do
-    'test'
+    erb :pay
+  end
+
+  get '/success' do
+    erb :success
+  end
+
+  post '/charge' do
+    Stripe::Charge.create(
+      :amount => 400,
+      :currency => "usd",
+      :source => params[:stripeToken], # obtained with Stripe.js
+      :metadata => {'email' => params[:stripeEmail]},
+      :description => "Charge for test@example.com"
+    )
+
+    redirect '/success'
   end
 end
